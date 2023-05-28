f1f0dea8a00f7e38d1bea14c41feb90c29354516
ka7833 <kaleab.admassu@my.ccsu.edu>
2023-05-27 22:28:56 -0600

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Hello From AWS\n')
+   response.end('Hello From AWS Week 3 Module\n')
 }).listen(3333)
 
 // Console will print the message
